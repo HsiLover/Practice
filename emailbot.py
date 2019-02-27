f6dcbc3d9dcdceaeb2aba3c6c7506325ea82a778
Hyuck <40300715+Hyuck-2@users.noreply.github.com>
2019-02-27 23:01:26 +0900

    basic emailbot made
    dependencies - BeautifulSoup and couple more default libraries

--- emailbot.py
+++ emailbot.py
@@ -14,22 +14,25 @@ while True:
     conn = sqlite3.connect(sql)
     cur = conn.cursor()
     cur.execute('CREATE TABLE IF NOT EXISTS email_harvest(id INTEGER NOT NULL UNIQUE PRIMARY KEY, source CHAR(200) NOT NULL, email CHAR(50) NOT NULL UNIQUE)')
-    cur.execute('CREATE TABLE IF NOT EXISTS webpages(id INTEGER NOT NULL UNIQUE PRIMARY KEY, links CHAR(200) NOT NULL UNIQUE)')
-    cur.execute('SELECT * FROM email_harvest ORDER BY id DESC LIMIT 1')
-    previous = cur.fetchall()
+    cur.execute('CREATE TABLE IF NOT EXISTS webpages(id INTEGER NOT NULL UNIQUE PRIMARY KEY, links CHAR(200) NOT NULL UNIQUE, visited INTEGER)')
     source = ''
-    if previous:
-        previous = previous[0]
-        cur.execute('SELECT id FROM webpages WHERE links=\'{}\''.format(previous[1]))
-        previous_cursor = cur.fetchall()[0][0]
-        cur.execute('SELECT links FROM webpages ORDER BY id ASC LIMIT 1 WHERE id > {}'.format(previous_cursor))
-        source = cur.fetchall()[0][0]
-    cur.close()
-    conn.close()
+
+    cur.execute('SELECT links FROM webpages WHERE visited = 0 ORDER BY id ASC LIMIT 1')
+    link = cur.fetchall()
+    if link:
+        source = link[0][0]
+        cur.execute('UPDATE webpages SET visited = 1 WHERE links = \'{}\''.format(source))
+
     if not source:
         source = input('Enter an url for me to start crawl around from: ')
+        cur.execute("INSERT OR IGNORE INTO webpages(links, visited) VALUES(\'{}\', 1)".format(source))
+
     if not source.startswith('https://'):
         source = 'https://' + source
+
+    cur.close()
+    conn.close()
+
     try:
         print('Connecting to the page...')
         page = requests.get(source)
@@ -38,9 +41,11 @@ while True:
     except:
         print('Somehting went wrong...')
         continue
+
     print('Looking at the webpage...')
     if isinstance(content, bytes):
         content = content.decode()
+
     print('Sorting out emails and links...')
     soup = BeautifulSoup(content, 'html.parser')
     links = set([x['href'] for x in soup.find_all('a') if x['href'].startswith('https://')])
@@ -48,27 +53,22 @@ while True:
     count_email = 0
     count_link = len(links)
 
+    conn = sqlite3.connect(sql)
+    cur = conn.cursor()
+
     for email in emails:
-        conn = sqlite3.connect(sql)
-        cur = conn.cursor()
         cur.execute("INSERT OR IGNORE INTO email_harvest(source, email) VALUES(\'{}\', \'{}\')".format(source[:200], email.lower()))
-        conn.commit()
-        cur.close()
-        conn.close()
         count_email += 1
-    print('{} emails have been collected...'.format(count_email))
 
     for link in links:
-        conn = sqlite3.connect(sql)
-        cur = conn.cursor()
-        print('='*20)
-        print(link)
-        print('='*20)
-        cur.execute("INSERT OR IGNORE INTO webpages(links) VALUES(\'{}\')".format(link))
-        conn.commit()
-        cur.close()
-        conn.close()
+        cur.execute("INSERT OR IGNORE INTO webpages(links, visited) VALUES(\'{}\', 0)".format(link))
 
-    print('{} links have been collected...'.format(count_link))
-    rand = random.randrange(10, 20)
+    conn.commit()
+    cur.close()
+    conn.close()
+
+    print('='*20)
+    print('{} emails, {} links have been collected from {}...'.format(count_email, count_link, source))
+    print('='*20)
+    rand = random.randrange(20, 30)
     time.sleep(rand)
