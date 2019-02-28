77659662a30efa38212ea03291c07ce86a3dcf21
Hyuck <40300715+Hyuck-2@users.noreply.github.com>
2019-02-28 15:00:10 +0900

    error control seems to be taking most of my time

--- emailbot.py
+++ emailbot.py
@@ -16,6 +16,7 @@ while True:
     cur.execute('CREATE TABLE IF NOT EXISTS email_harvest(id INTEGER NOT NULL UNIQUE PRIMARY KEY, source CHAR(200) NOT NULL, email CHAR(50) NOT NULL UNIQUE)')
     cur.execute('CREATE TABLE IF NOT EXISTS webpages(id INTEGER NOT NULL UNIQUE PRIMARY KEY, links CHAR(200) NOT NULL UNIQUE, visited INTEGER)')
     source = ''
+    flag = False
 
     cur.execute('SELECT links FROM webpages WHERE visited = 0 ORDER BY id ASC LIMIT 1')
     link = cur.fetchall()
@@ -27,28 +28,60 @@ while True:
         source = input('Enter an url for me to start crawl around from: ')
         cur.execute("INSERT OR IGNORE INTO webpages(links, visited) VALUES(\'{}\', 1)".format(source))
 
-    if not source.startswith('https://'):
+    if not(source.startswith('https://') or source.startswith('http://')):
         source = 'https://' + source
 
-    cur.close()
-    conn.close()
-
     try:
-        print('Connecting to the page...')
+        print('Connecting to {}...'.format(source))
         page = requests.get(source)
         content = page.content
         print(content[:50])
     except:
         print('Somehting went wrong...')
+        cur.execute('UPDATE webpages SET visited = 1 WHERE links = \'{}\''.format(source))
+        flag = True
+        time.sleep(10)
+
+    conn.commit()
+    cur.close()
+    conn.close()
+
+    if flag:
         continue
 
     print('Looking at the webpage...')
     if isinstance(content, bytes):
-        content = content.decode()
+        try: content = content.decode()
+        except:
+            conn = sqlite3.connect(sql)
+            cur = conn.cursor()
+            cur.execute('UPDATE webpages SET visited = 1 WHERE links = \'{}\''.format(source))
+            conn.commit()
+            cur.close()
+            conn.close()
+            continue
 
     print('Sorting out emails and links...')
     soup = BeautifulSoup(content, 'html.parser')
-    links = set([x['href'] for x in soup.find_all('a') if x['href'].startswith('https://')])
+    soup_content = soup.find_all('a')
+    links = list()
+    divs = soup.find_all('div')
+    re_links = list()
+    for div in divs:
+        re_links.append(div.text)
+
+    re_links = re.findall(url_re, ' '.join(re_links))
+
+    for link in soup_content:
+        try:
+            hyperlink = link['href']
+            if hyperlink.startswith('https://') or hyperlink.startwith('http://'):
+                links.append(hyperlink)
+        except:
+            continue
+
+    links += re_links
+    links = set(links)
     emails = re.findall(email_re, content)
     count_email = 0
     count_link = len(links)
@@ -70,5 +103,5 @@ while True:
     print('='*20)
     print('{} emails, {} links have been collected from {}...'.format(count_email, count_link, source))
     print('='*20)
-    rand = random.randrange(20, 30)
+    rand = random.randrange(40, 60)
     time.sleep(rand)
